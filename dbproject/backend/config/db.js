d8287626af79dc506a7e12be3c75b6114fae6a62
Khazin-Awan <khazinawan25@gmail.com>
2025-04-19 21:20:44 +0500

    LESSGOOO ani paa di

--- dbproject/backend/config/db.js
+++ dbproject/backend/config/db.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
+//require('dotenv').config({ path: __dirname + '/../.env' });
+
 const sql = require('mssql');
 
+console.log("ENV values:");
+console.log("User:", process.env.DB_USER);
+console.log("Password:", process.env.DB_PASSWORD);
+console.log("Server:", process.env.DB_SERVER);
+console.log("Database:", process.env.DB_NAME);
 
 // Database configuration
 const dbConfig = {
@@ -8,7 +15,8 @@ const dbConfig = {
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
   database: process.env.DB_NAME,
-  // port: 1433, 
+  port: 1433, 
+
   options: {
     encrypt: false, // Set to true if using Azure
     enableArithAbort: true,
@@ -24,7 +32,15 @@ const poolPromise = new sql.ConnectionPool(dbConfig)
     console.log('Connected to MSSQL');
     return pool;
   })
-  .catch(err => console.log('Database Connection Failed!', err));
+  .catch(err => {
+console.error('Database Connection Failed!', err.code, err.message, err);
+  });
 
 
 module.exports = { sql, poolPromise };
+
+
+// sql.connect(dbConfig)
+//   .then(pool => pool.request().query('SELECT 1 AS test'))
+//   .then(result => console.log(result.recordset))
+//   .catch(err => console.error('Connection error:', err));
